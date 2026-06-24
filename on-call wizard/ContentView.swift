7afbf8d12c5df977ce6100694af3d320ecb30594
Edward <erdunn706@gmail.com>
2026-06-23 19:16:52 -0600

    Initial Commit

---
+++ on-call wizard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  on-call wizard
+//
+//  Created by Edward Dunn on 6/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ on-call wizard/on_call_wizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  on_call_wizardApp.swift
+//  on-call wizard
+//
+//  Created by Edward Dunn on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct on_call_wizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
